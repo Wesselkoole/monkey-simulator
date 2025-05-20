4719d234dd6c8892d5d8881e3cd04b114420131e
wiresnchains <dgrachov@icloud.com>
2025-05-20 12:00:37 +0200

    Multiply banana yield by the amount of monkeys that the player has

--- src/actions/MonkeyActions.jsx
+++ src/actions/MonkeyActions.jsx
@@ -12,6 +12,6 @@ export function monkeyClicked(userContext) {
 
     setGameData({
         currentMonkeyClimbHeight: 0,
-        bananas: gameData.bananas + gameData.bananaTreeYield
+        bananas: gameData.bananas + (gameData.bananaTreeYield * gameData.monkeys)
     });
 }

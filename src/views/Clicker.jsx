be23149eafbc1b986926d323f082a87a3b4cb15c
Wessel <wkoole@student.scalda.nl>
2025-05-20 15:09:30 +0200

    clicker functie gekoppeld en nav verbeterd

--- src/views/Clicker.jsx
+++ src/views/Clicker.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { useUserContext } from '../hooks/UserContext'
+import { monkeyClicked } from '../actions/MonkeyActions';
 
 const Clicker = () => {
+    const userContext = useUserContext();
+
+    const handleClick = () => {
+        monkeyClicked(userContext);
+    };
+
     return (
         <div className="d-flex justify-content-center align-items-center h-100">
-            <img className="img-fluid" src="src/images/monkey.png" alt="Monkey" />
+            <img
+                className="img-fluid"
+                src="src/images/monkey.png"
+                alt="Monkey"
+                style={{ cursor: 'pointer' }}
+                onClick={handleClick}
+            />
         </div>
     );
 };
--- src/views/Nav.jsx
+++ src/views/Nav.jsx
@@ -1,18 +1,39 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { useUserContext } from '../hooks/UserContext';
 
 const Nav = () => {
-    return (
+    const userContext = useUserContext();
 
-        <nav className="navbar bg-warning">
-            <div className="container-fluid">
-                    <a className="navbar-brand text-start"   style={{ height: "5%", width: "5%" }} href="#"><img className="img-fluid" alt="logo" src="src/images/logo.png"/> </a>
-                <div className="col-md-11">
-                    <h5 className="text-center">Banana counter:</h5>
-                </div>
+    return (
+        <nav className="navbar bg-warning shadow-sm py-2">
+            <div className="container-fluid d-flex justify-content-between align-items-center">
+                {/* Logo */}
+                <a className="navbar-brand d-flex align-items-center" href="#">
+                    <img
+                        src="src/images/logo.png"
+                        alt="Logo"
+                        className="img-fluid"
+                        style={{ height: '40px', width: 'auto' }}
+                    />
+                </a>
             </div>
-        </nav>    );
+
+            {/* Banana Counter */}
+            <div
+                className="position-absolute top-50 start-50 translate-middle d-flex align-items-center gap-2"
+                style={{ pointerEvents: 'none' }}
+            >
+                <img
+                    src="src/images/bananen.png"
+                    alt="Bananen"
+                    style={{ height: '30px', width: 'auto' }}
+                />
+                <h5 className="mb-0">{userContext.gameData.bananas}</h5>
+            </div>
+        </nav>
+    );
 };
 
 export default Nav;

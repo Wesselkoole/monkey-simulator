9036b4268f218b1c3a88b276ea881a162bfa2bf2
wiresnchains <dgrachov@icloud.com>
2025-05-20 11:20:55 +0200

    Fix current monkey climb height update

--- src/actions/MonkeyActions.jsx
+++ src/actions/MonkeyActions.jsx
@@ -2,12 +2,16 @@ export function monkeyClicked(userContext) {
     const gameData = userContext.gameData;
     const setGameData = userContext.setGameData;
 
-    gameData.setCurrentMonkeyClimbHeight(gameData.currentMonkeyClimbHeight + 1);
+     setGameData({
+        currentMonkeyClimbHeight: gameData.currentMonkeyClimbHeight + 1
+    });
+
     if (gameData.currentMonkeyClimbHeight < gameData.maxTreeClimbHeight) {
         return;
     }
+
     setGameData({
         currentMonkeyClimbHeight: 0,
         bananas: gameData.bananas + gameData.bananaTreeYield
-    })
+    });
 }

ec2348d5d9c5b1b765dd6810f9e8cb23ccef9436
Wesselkoole <104504409+Wesselkoole@users.noreply.github.com>
2025-05-21 21:24:51 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/monkey-clicker/',
+  base: '/monkey-simulator/',
   plugins: [react()],
 });

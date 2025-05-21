d0e676cfe0f6e515291f81adca901a849edb8bf7
wiresnchains <dgrachov@icloud.com>
2025-05-21 12:57:55 +0200

    Add tree & monkeys upgrades

--- src/hooks/UserContext.jsx
+++ src/hooks/UserContext.jsx
@@ -14,7 +14,7 @@ export function useUserContext() {
 
 export function UserContextProvider(props) {
     const [gameData, setGameData] = useState({
-        bananas: 0,
+        bananas: 99999999,
         trees: [
             {
                 monkeys: 1
--- src/views/Clicker.jsx
+++ src/views/Clicker.jsx
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { useUserContext } from '../hooks/UserContext';
-import { monkeyClicked } from '../actions/MonkeyActions';
+import { monkeyClicked, upgradeMonkeys, upgradeTree } from '../actions/MonkeyActions';
 
 const Clicker = () => {
     const userContext = useUserContext();
@@ -9,6 +9,8 @@ const Clicker = () => {
     return (
         <div className="d-flex justify-content-center align-items-center h-100">
             <img className="img-fluid" src="src/images/monkey.png" alt="Monkey" onClick={() => monkeyClicked(userContext)} />
+            <button onClick={() => upgradeMonkeys(userContext)}>Upgrade monkeys</button>
+            <button onClick={() => upgradeTree(userContext)}>Upgrade tree</button>
         </div>
     );
 };
